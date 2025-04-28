c0cd6d502e0ca6ce770785cf4ee9962ecc7c579c
mohamedsayed18 <mohamed95.a.s@gmail.com>
2025-04-28 20:45:45 +0300

    API: Create a simple unit test for login

--- api_mvp/myapi/tests.py
+++ api_mvp/myapi/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+import json
 
-# Create your tests here.
+
+class TestApi(TestCase):
+    def test_login_success(self):
+        url = 'http://localhost:8000/myapi/login/'
+        data = {
+            'username': 'admin',
+            'password': 'secret'
+        }
+        response = self.client.post(url, data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
