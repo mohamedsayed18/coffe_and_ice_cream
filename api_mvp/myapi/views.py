ca47ae0cc43c86560af7b1d85f6703f075d1c5e9
mohamedsayed18 <mohamed95.a.s@gmail.com>
2025-04-28 21:53:58 +0300

    API: basic change sate request

--- api_mvp/myapi/views.py
+++ api_mvp/myapi/views.py
@@ -1,8 +1,17 @@
 # from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+
+system_state = 'Login'
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def login(request):
-    return JsonResponse({'token': 'abc123'})
+    if system_state == 'dashboard':
+        return JsonResponse({'token': 'abc123'})
+    else:
+        return JsonResponse({'token': '789'})
+
+def change_system_state(request):
+    return JsonResponse({'state': system_state})
