151ffb04e4efd8d2e75350558f5e8fc511381d7c
mohamedsayed18 <mohamed95.a.s@gmail.com>
2025-04-28 17:35:58 +0300

    Test in-range negative value

---
+++ Math_function/test_discontinuous_function.py
@@ -0,0 +1,11 @@
+import unittest
+
+from discontinuous_function import discontinuous
+# TODO naming convenction 
+# TODO handle rational numbers
+# TODO test fractions
+
+class TestDiscontinuousFunction(unittest.TestCase):
+    def test_negative_range(self):
+        result = discontinuous(-5)
+        self.assertEqual(result, -25)
