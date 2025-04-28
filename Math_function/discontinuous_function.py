675979636159b543c80540268e446dd779b62b46
mohamedsayed18 <mohamed95.a.s@gmail.com>
2025-04-28 17:30:18 +0300

    Implement the discontinuous function

---
+++ Math_function/discontinuous_function.py
@@ -0,0 +1,13 @@
+# TODO make a plot
+# TODO number 10 is not handled
+
+def discontinuous(x: float) -> float:
+    if x == -1:
+        # TODO raise exception
+        pass
+    elif -10 <= x <= -2:
+        return -(x**2)
+    elif -2 < x <= 9:
+        return (1 - x) / (1 + x)
+    elif 10 < x <= 35:
+        return abs(x - 12)
