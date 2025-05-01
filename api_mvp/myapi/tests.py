ff949509ddf80aecb038fe86995766f5104d07f5
mohamedsayed18 <mohamed95.a.s@gmail.com>
2025-05-01 09:59:35 +0300

    API: Test login failure and change system state failure

--- api_mvp/myapi/tests.py
+++ api_mvp/myapi/tests.py
@@ -8,6 +8,17 @@ class TestApi(TestCase):
     def setUp(self):
         models.SystemSettings.objects.create(settings='system_state', value='Log in')
 
+    def get_system_state(self) -> str:
+        url = 'http://localhost:8000/myapi/state/'
+        response = self.client.get(url)
+        response_data = json.loads(response.content)
+        return response_data['state']
+    
+    def set_system_state(self, state: str) -> None:
+        system_state = models.SystemSettings.objects.get(settings='system_state')
+        system_state.value = state
+        system_state.save()
+
     def test_login_success(self):
         url = 'http://localhost:8000/myapi/login/'
         data = {
@@ -16,10 +27,7 @@ class TestApi(TestCase):
         }
         response = self.client.post(url, data=json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        url = 'http://localhost:8000/myapi/state/'
-        response = self.client.get(url)
-        response_data = json.loads(response.content)
-        self.assertEqual(response_data['state'], 'Dashboard')
+        self.assertEqual(self.get_system_state(), 'Dashboard')
 
     def test_login_failure(self):
         url = 'http://localhost:8000/myapi/login/'
@@ -29,16 +37,10 @@ class TestApi(TestCase):
         }
         response = self.client.post(url, data=json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 401)
-
-        url = 'http://localhost:8000/myapi/state/'
-        response = self.client.get(url)
-        response_data = json.loads(response.content)
-        self.assertEqual(response_data['state'], 'Log in')
+        self.assertEqual(self.get_system_state(), 'Log in')
 
     def test_change_system_state(self):
-        system_state = models.SystemSettings.objects.get(settings='system_state')
-        system_state.value = 'Dashboard'
-        system_state.save()
+        self.set_system_state('Dashboard')
 
         url = 'http://localhost:8000/myapi/state/'
         data = {'new_state': 'Item Details'}
@@ -46,10 +48,15 @@ class TestApi(TestCase):
         response_data = json.loads(response.content)
         self.assertEqual(response_data['new_state'], 'Item Details')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_system_state(), 'Item Details')
 
-        response = self.client.get(url)
-        response_data = json.loads(response.content)
-        self.assertEqual(response_data['state'], 'Item Details')
+    def test_change_system_state_wrong_transition(self):
+        self.set_system_state('Item Details')
+
+        url = 'http://localhost:8000/myapi/state/'
+        data = {'new_state': 'Config'}
+        response = self.client.post(url, data=json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 409)
 
     def test_change_state_failure(self):
         url = 'http://localhost:8000/myapi/state/'
